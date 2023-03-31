a5ffa712f5de3f119b306992d842abd825083593
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2023-03-31 23:35:52 +0530

    header done along with styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,12 @@ import '../style/header.css'
 export default function Header() {
     return (
         <div id='header'>
-            
+            <h1 id='lost-heading'>Perierat</h1>
+            <h1 id='found-heading'>Inventum</h1>
+            <input type="text" placeholder='Search' autoFocus />
+            <img src="icons/search.svg" alt="loading" />
+            <button id='lost-button' type='button'>Lost</button>
+            <button id='found-button' type='button'>Found</button>
         </div>
     )
 }
