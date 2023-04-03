b1bb07fcd1b5de403ed8188e7927faa6607027c0
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2023-04-03 19:34:46 +0530

    add item completed with styling

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import AddItem from './components/Item/AddItem'
 function App() {
 
   const lost = {
+    category: "Lost",
+    person: "Owner",
     button: "#DC3545",
     background: "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)"
   }
   const found = {
+      category: "Found",
+      person: "Finder",
       button: "#157347",
       background: "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)"
   }
-  const color = [lost, found]
 
   return (
       <Router>
         <Header />
-        <Home color={color}/>
+        {/* <Home color={[lost, found]}/> */}
+        {/* <AddItem color={found}/> */}
+        <AddItem color={lost}/>
       </Router>
   );
 }
--- src/components/Item/AddItem.jsx
+++ src/components/Item/AddItem.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-export default function AddItem() {
+import '../../style/add-item.css'
+
+export default function AddItem(props) {
     return (
-        <>
-            
-        </>
+        <div id='add-item' style={{background: props.color.background}}>
+            <div id='ai-item'>
+                <h1>Item {props.color.category}</h1>
+                <label htmlFor="item"><h2>Item</h2></label>
+                <input type="text" name='item' placeholder='Enter item' autoFocus />
+                <label htmlFor="place"><h2>Place</h2></label>
+                <input type="text" name='place' placeholder='Enter place' />
+                <label htmlFor="description"><h2>Description</h2></label>
+                <textarea name="description" placeholder='Enter description' cols="32" rows="4"></textarea>
+            </div>
+            <div id='ai-person'>
+                <h1>{props.color.person}</h1>
+                <label htmlFor="name"><h2>Name</h2></label>
+                <input type="text" name='name' placeholder='Enter name' />
+                <label htmlFor="phone"><h2>Phone Number</h2></label>
+                <input type="tel" name='phone' placeholder='Enter phone number' maxLength={10} />
+                <label htmlFor="email"><h2>Email</h2></label>
+                <input type="email" name='email' placeholder='Enter email' />
+            </div>
+            <div id='ai-image'>
+                <h1>Upload Image</h1>
+                <input type="file" />
+            </div>
+            <div id='ai-button'>
+                <button type='button' style={{background: props.color.button, border: props.color.button}}>Add</button>
+                <button type='button' style={{background: props.color.button, border: props.color.button}}>Cancel</button>
+            </div>
+        </div>
     )
 }
